0781b5f83460d87786ea804f58d08fc7227dc7c8
kscsq <kscsq@mail.ru>
2017-09-26 15:43:01 +0300

    Чат (9)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -9,19 +9,36 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Server {
     private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
-    public static void sendBroadcastMessage(Message message){
-        for (Map.Entry<String,Connection> m : connectionMap.entrySet())
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> m : connectionMap.entrySet())
             try {
                 m.getValue().send(message);
             } catch (IOException e) {
                 ConsoleHelper.writeMessage("Message was not delivered!");
             }
     }
+
     private static class Handler extends Thread {
         private Socket socket;
 
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            Message answer;
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                answer = connection.receive();
+                if (answer.getType() == MessageType.USER_NAME)
+                    if (!answer.getData().isEmpty())
+                        if (!connectionMap.containsKey(answer.getData())) {
+                            connectionMap.put(answer.getData(), connection);
+                            break;
+                        }
+            }
+            connection.send(new Message(MessageType.NAME_ACCEPTED));
+            return answer.getData();
         }
     }
 
