cfc314a852626f7b4b0e46f8d9a3d71f97647174
kscsq <kscsq@mail.ru>
2017-09-26 12:48:22 +0300

    Чат (8)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,9 +1,22 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message){
+        for (Map.Entry<String,Connection> m : connectionMap.entrySet())
+            try {
+                m.getValue().send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Message was not delivered!");
+            }
+    }
     private static class Handler extends Thread {
         private Socket socket;
 
