c45510ce2d2b98a8853041d893c7e2227239a61f
kscsq <kscsq@mail.ru>
2017-09-26 12:31:14 +0300

    Чат (7)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,26 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Input server port:");
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Server started...");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Something wrong, Server socket closed.");
+        }
     }
 }
