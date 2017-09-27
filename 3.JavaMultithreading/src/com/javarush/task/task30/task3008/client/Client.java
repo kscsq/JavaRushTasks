85e7f1633eb3c107c83a90161ade6875c2bf7299
kscsq <kscsq@mail.ru>
2017-09-27 22:28:05 +0300

    Чат (14)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,11 +1,44 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
     public class SocketThread extends Thread{
 
+    }
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Input server address:");
+        return ConsoleHelper.readString();
+    }
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Please enter port number: ");
+        return ConsoleHelper.readInt();
+    }
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Please enter user name: ");
+        return ConsoleHelper.readString();
+    }
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Message delivery error");
+            clientConnected = false;
+        }
     }
 }
