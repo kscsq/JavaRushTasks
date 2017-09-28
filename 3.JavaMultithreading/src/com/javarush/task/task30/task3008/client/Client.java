1b9cc102586c70f2c2d3a26660e508c11c95f54c
kscsq <kscsq@mail.ru>
2017-09-28 16:20:12 +0300

    Чат (17)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -15,6 +15,7 @@ public class Client {
         Client client = new Client();
         client.run();
     }
+
     public void run() {
         SocketThread socketThread = getSocketThread();
         socketThread.setDaemon(true);
@@ -42,19 +43,49 @@ public class Client {
     }
 
     public class SocketThread extends Thread {
-        protected void processIncomingMessage(String message){
+        protected void processIncomingMessage(String message) {
             ConsoleHelper.writeMessage(message);
         }
-        protected void informAboutAddingNewUser(String userName){
+
+        protected void informAboutAddingNewUser(String userName) {
             ConsoleHelper.writeMessage(userName + " has just joined chat.");
         }
-        protected void informAboutDeletingNewUser(String userName){
+
+        protected void informAboutDeletingNewUser(String userName) {
             ConsoleHelper.writeMessage(userName + " has just left chat.");
         }
-        protected void notifyConnectionStatusChanged(boolean clientConnected){
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
             synchronized (Client.this) {
                 Client.this.clientConnected = clientConnected;
                 Client.this.notify();
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            Message message;
+            while (true) {
+                message = connection.receive();
+                if (message.getType() == MessageType.NAME_REQUEST) {
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+                } else if (message.getType() == MessageType.NAME_ACCEPTED) {
+                    notifyConnectionStatusChanged(true);
+                    break;
+                }else throw new IOException("Unexpected MessageType");
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException{
+            Message message;
+            while (true){
+                message = connection.receive();
+                if (message.getType() == MessageType.TEXT)
+                    processIncomingMessage(message.getData());
+                else if (message.getType() == MessageType.USER_ADDED)
+                    informAboutAddingNewUser(message.getData());
+                else if (message.getType() == MessageType.USER_REMOVED)
+                    informAboutDeletingNewUser(message.getData());
+                else throw new IOException("Unexpected MessageType");
             }
         }
     }
